cfb4055c5b459c13a1f71d89a79b41a1aea47f24
ZYAD-19 <zyadmahmod19720@gmail.com>
2024-05-25 23:53:07 +0300

    first commit

---
+++ DVLD Database Layer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Database_Layer
+{
+    public class Class1
+    {
+    }
+}
